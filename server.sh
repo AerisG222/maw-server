56448023adc86c89973b1ed15b36dff9cf6ead17
Mike Morano <mmorano@mikeandwan.us>
2025-10-12 12:50:10 -0400

    updates to get this working against chocobo

--- server.sh
+++ server.sh
@@ -2,5 +2,6 @@
 
 ansible-playbook \
     --become-password-file "~/maw-server/ansible/become-password-file" \
+    --connection-password-file "~/maw-server/ansible/connection-password-file" \
     --inventory "inventories/prod.yml" \
     server-playbook.yml
